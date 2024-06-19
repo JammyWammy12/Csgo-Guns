3b5a9b8985940b94a7c8ae77b9414dd6579211be
james <21133@wc.school.nz>
2024-06-19 13:52:57 +1200

    I imported my csgo data, and displayed it on my website.

--- app.py
+++ app.py
@@ -1,6 +1,17 @@
 from flask import Flask, render_template
 
 app = Flask(__name__)
+DATABASE = "csgoguns.db"
+import sqlite3
+from sqlite3 import Error
+def create_connect(db_filename):
+    try:
+        conn = sqlite3.connect(db_filename)
+        return conn
+    except Error as e:
+        print(e)
+        return None
+
 
 
 @app.route('/')
@@ -10,13 +21,33 @@ def render_home_page():  # put application's code here
 
 @app.route('/t_guns')
 def render_terrorist_page():  # put application's code here
-    return render_template('ct_guns.html')
+
+    query = "SELECT name FROM csgo_guns WHERE terrorist = TRUE"
+    conn = create_connect(DATABASE)
+    cursor = conn.cursor()
+    cursor.execute(query, )
+
+    data_list = cursor.fetchall()
+    print(data_list)
+
+    return render_template('t_guns.html', data=data_list)
+
 
 
 
 @app.route('/ct_guns')
 def render_counter_terrorist_page():  # put application's code here
-    return render_template('t_guns.html')
+
+    query = "SELECT name FROM csgo_guns WHERE counter_terrorist = TRUE"
+    conn = create_connect(DATABASE)
+    cursor = conn.cursor()
+    cursor.execute(query, )
+
+    data_list = cursor.fetchall()
+    print(data_list)
+
+
+    return render_template('ct_guns.html', data=data_list)
 
 
 
